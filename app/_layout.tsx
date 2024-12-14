8944838e66b1ae2d86dbd2dccbbc75354bb654e5
Irshad Nazree <inazree370@gmail.com>
2024-12-14 22:35:36 +0800

    feat: replace existing font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SatoshiRegular: require('../assets/fonts/Satoshi-Regular.ttf'),
+    SatoshiBold: require('../assets/fonts/Satoshi-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,17 @@
-import { Text, View } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from 'react-native';
+import 'react-native-reanimated';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
